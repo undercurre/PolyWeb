bc5414a5a02cc6fe97ce75fb742e839f0822768b
undercurre <undercurre@163.com>
2023-08-12 23:36:05 +0800

    chore(utils): 树形解析工具修正

--- src/views/UtilView.vue
+++ src/views/UtilView.vue
@@ -2,6 +2,8 @@
 	<UseTypeOf></UseTypeOf>
 	<UsePattern></UsePattern>
 	<UseStorage></UseStorage>
+	<UseNumber></UseNumber>
+	<UseTreeHelper></UseTreeHelper>
 </template>
 
 <script lang="ts" setup></script>
