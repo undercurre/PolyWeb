d1038c119dbc86bba1984814e004327081c6af62
undercurre <undercurre@163.com>
2023-08-24 01:30:55 +0800

    feat: postcss集成完毕

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
-import preset from './preset/preset';
+import preset from "./preset/preset";
+import pxToViewport from "postcss-px-to-viewport-8-plugin";
+import postcssPresetEnv from "postcss-preset-env";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [...preset],
+  css: {
+    postcss: {
+      plugins: [
+        pxToViewport({
+          viewportWidth: 1280,
+          unitPrecision: 6,
+          unitToConvert: "px",
+          propList: ["*"],
+        }),
+        postcssPresetEnv({
+          stage: 3, // 使用的 CSS 语法阶段
+        }),
+      ],
+    },
+  },
 });
