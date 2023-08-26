54b069b4e558fb9bec373cfcb187a63294850d07
undercurre <undercurre@163.com>
2023-08-26 18:50:45 +0800

    feat：环境组件

---
+++ src/components/useEnv.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p v-if="isDevelopment">当前是开发环境</p>
+    <p v-if="isProduction">当前是生产环境</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isDevelopment = import.meta.env.DEV;
+const isProduction = import.meta.env.PROD;
+
+// 在这里可以根据环境变量进行不同的操作
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,6 +26,7 @@ const openVn = () => {
 		<UseI18n></UseI18n>
 		<UseVueuse></UseVueuse>
 		<UseFetch></UseFetch>
+		<UseEnv></UseEnv>
 		<SvgIcon name="vue"></SvgIcon>
 		<SvgIcon name="test-icon1"></SvgIcon>
 	</div>
