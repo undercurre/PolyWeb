3053bf3f530d6d11933bd93ef225c33e8c509551
undercurre <undercurre@163.com>
2023-08-26 21:42:56 +0800

    feat: 更新env展示组件

--- src/components/useEnv.vue
+++ src/components/useEnv.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
-    <p v-if="isDevelopment">当前是开发环境</p>
-    <p v-if="isProduction">当前是生产环境</p>
-  </div>
+	<div>
+		<div>当前环境：{{ envObj.VITE_SERVICE_ENV }}</div>
+	</div>
 </template>
 
 <script setup lang="ts">
-const isDevelopment = import.meta.env.DEV;
-const isProduction = import.meta.env.PROD;
+const envObj = import.meta.env;
 
 // 在这里可以根据环境变量进行不同的操作
 </script>
