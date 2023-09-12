847043db3e6202dc6762b792a67bff2999ccf96a
lirh42 <undercurre@163.com>
2023-09-12 13:33:09 +0800

    feat(utils): 请求中更换自己造的adapter

--- src/components/useFetch.vue
+++ src/components/useFetch.vue
@@ -10,73 +10,11 @@ import axios, {
 	type InternalAxiosRequestConfig
 } from 'axios';
 
+import uniAdapter from 'uni-axios-adapter-all';
+
 const responseShow = ref('');
 
-const getResponse = (
-	res: UniApp.RequestSuccessCallbackResult | UniApp.GeneralCallbackResult,
-	config: InternalAxiosRequestConfig
-): AxiosResponse => {
-	let statusCode = 401;
-	let header = null;
-	let data = null;
-	if ('statusCode' in res) {
-		statusCode = res.statusCode;
-		header = res.header;
-		data = res.data;
-	}
-	const { errMsg } = res;
-	const response: AxiosResponse = {
-		...res,
-		data: data,
-		status: statusCode,
-		statusText: errMsg ?? 'No Msg',
-		headers: header,
-		config,
-		request: null
-	};
-
-	return response;
-};
-
-axios.defaults.adapter = (config: InternalAxiosRequestConfig) => {
-	if (!uni) {
-		throw new Error('please use this in uni-app project!');
-	}
-	return new Promise((resolve, reject) => {
-		const { baseURL, url, headers, data, params, method } = config;
-
-		const uniConfig: UniApp.RequestOptions = {
-			url: (baseURL || '') + url,
-			data: data, // 可选
-			header: headers, // 可选
-			method: ['OPTIONS', 'GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'TRACE', 'CONNECT'].includes(
-				method === undefined ? 'GET' : method
-			)
-				? (method as 'OPTIONS' | 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'TRACE' | 'CONNECT')
-				: 'GET', // 可选
-			timeout: config.timeout // 可选// 可选
-		};
-
-		if (data || params) {
-			try {
-				uniConfig.data = JSON.parse(data || params);
-			} catch (e) {
-				uniConfig.data = data || params;
-			}
-		}
-		uni.request({
-			...uniConfig,
-			success(res) {
-				const response = getResponse(res, config);
-				resolve(response);
-			},
-			fail(res) {
-				const response = getResponse(res, config);
-				reject(response);
-			}
-		});
-	});
-};
+axios.defaults.adapter = uniAdapter;
 
 function handleLoginBtn() {
 	// 定义请求配置
