40aa2b8412bd38a0e2d577097bb8aabbccde2b47
lirh42 <undercurre@163.com>
2023-09-14 20:42:33 +0800

    feat(components): fetch小程序适配完成

--- src/pages/fetch/index.vue
+++ src/pages/fetch/index.vue
@@ -6,10 +6,13 @@
 
 <script setup lang="ts">
 import axios, {
+	type AxiosInstance,
 	type AxiosRequestConfig,
 	type AxiosResponse,
 	type InternalAxiosRequestConfig
 } from 'axios';
+import { method } from 'lodash-es';
+import { config } from 'process';
 
 import uniAdapter from 'uni-axios-adapter-all';
 
@@ -19,9 +22,14 @@ const platform = ref();
 
 platform.value = uni.getSystemInfoSync().platform;
 
+let axiosInstance: AxiosInstance;
+
 switch (uni.getSystemInfoSync().platform) {
 	case 'android':
-		axios.defaults.adapter = uniAdapter;
+		axiosInstance = axios.create({
+			baseURL: 'https://jsonplaceholder.typicode.com',
+			adapter: uniAdapter // 使用自定义适配器
+		});
 		console.log('运行Android上');
 		break;
 	case 'ios':
@@ -31,19 +39,18 @@ switch (uni.getSystemInfoSync().platform) {
 	case 'windows':
 		console.log('运行Windows上');
 	default:
+		axiosInstance = axios.create({
+			baseURL: 'https://jsonplaceholder.typicode.com',
+			adapter: uniAdapter // 使用自定义适配器
+		});
 		console.log('运行在开发者工具上');
 		break;
 }
 
 function handleLoginBtn() {
-	// 定义请求配置
-	const requestOptions = {
-		url: 'https://jsonplaceholder.typicode.com/posts/1', // 替换成你要请求的 URL
-		method: 'GET'
-	};
-
 	// 发送 GET 请求
-	axios(requestOptions)
+	axiosInstance
+		.get('/posts/1')
 		.then((response) => {
 			// 请求成功处理
 			responseShow.value = JSON.stringify(response.data);
