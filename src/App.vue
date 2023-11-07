d9308e3423fa411381ef017530d678380efca7d9
undercurre <undercurre@163.com>
2023-11-08 01:34:10 +0800

    feat(views): 还原完成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
-import logo from '@/assets/logo.png';
+// import logo from '@/assets/logo.png';
 </script>
 
 <template>
-	<div class="flex flex-col items-center">
-		<img class="w-125px h-125px block mt-0 mb-2em mx-auto" :src="logo" />
-		<nav class="w-full text-12px text-center mt-2em">
-			<RouterLink to="/">Home</RouterLink>
-			<RouterLink to="/about">About</RouterLink>
-			<RouterLink to="/hooks">Hooks</RouterLink>
-			<RouterLink to="/utils">Utils</RouterLink>
-		</nav>
+	<div>
 		<RouterView />
 	</div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,240 @@
-<script setup lang="ts">
-import { showToast } from 'vant';
-import 'vant/es/toast/style';
-
-const openVn = () => {
-	ElMessage({
-		message: h('p', null, [h('span', null, '不需要手动引入并引入css')])
-	});
-};
-</script>
-
 <template>
-	<div>
-		<h3 class="text-left">Based on Vue3+TS+Vite</h3>
-		<h4 class="text-10px text-left">using Vue-router</h4>
-		<useStore></useStore>
-		<useAutoImport></useAutoImport>
-		<van-button @click="showToast('记得部分组件要手动引入并引入css')" class="mt-10px" type="success"
-			>Vant 成功按钮</van-button
-		>
-		<el-button @click="openVn" class="mt-10px ml-10px" size="large" type="primary"
-			>Element Plus</el-button
-		>
-		<h4 class="text-20px text-left text-amber">Using Iconify</h4>
-		<UseIconify></UseIconify>
-		<UseI18n></UseI18n>
-		<UseVueuse></UseVueuse>
-		<UseFetch></UseFetch>
-		<UseEnv></UseEnv>
-		<SvgIcon name="vue"></SvgIcon>
-		<SvgIcon name="test-icon1"></SvgIcon>
+	<div class="boxs">
+		<div class="maskLoading" v-if="isLoading">
+			<div class="loading">
+				<div :style="{ width: loadingWidth + '%' }"></div>
+			</div>
+			<div style="padding-left: 10px">{{ parseInt(loadingWidth) }}%</div>
+		</div>
+		<div class="mask">
+			<p>x : {{ x }} y:{{ y }} z :{{ z }}</p>
+			<button @click="isAutoFun">转动车</button>
+			<button @click="stop">停止</button>
+			<div class="flex">
+				<div
+					@click="setCarColor(index)"
+					v-for="(item, index) in colorAry"
+					:key="item"
+					:style="{ backgroundColor: item }"
+				></div>
+			</div>
+		</div>
 	</div>
 </template>
+
+<script setup>
+import { onMounted, reactive, ref, toRefs } from 'vue';
+import {
+	Color,
+	DirectionalLight,
+	DirectionalLightHelper,
+	HemisphereLight,
+	HemisphereLightHelper,
+	PerspectiveCamera,
+	Scene,
+	WebGLRenderer
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+//车身颜色数组
+const colorAry = [
+	'rgb(216, 27, 67)',
+	'rgb(142, 36, 170)',
+	'rgb(81, 45, 168)',
+	'rgb(48, 63, 159)',
+	'rgb(30, 136, 229)',
+	'rgb(0, 137, 123)',
+	'rgb(67, 160, 71)',
+	'rgb(251, 192, 45)',
+	'rgb(245, 124, 0)',
+	'rgb(230, 74, 25)',
+	'rgb(233, 30, 78)',
+	'rgb(156, 39, 176)',
+	'rgb(0, 0, 0)'
+]; // 车身颜色数组
+const loader = new GLTFLoader(); //引入模型的loader实例
+const defaultMap = {
+	x: 510,
+	y: 128,
+	z: 0
+}; // 相机的默认坐标
+const map = reactive(defaultMap); //把相机坐标设置成可观察对象
+const { x, y, z } = toRefs(map); //输出坐标给模板使用
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let scene, camera, renderer, controls, floor, dhelper, hHelper, directionalLight, hemisphereLight; // 定义所有three实例变量
+let isLoading = ref(true); //是否显示loading  这个load模型监听的进度
+let loadingWidth = ref(0); // loading的进度
+
+//创建灯光
+const setLight = () => {
+	directionalLight = new DirectionalLight(0xffffff, 0.5);
+	directionalLight.position.set(-4, 8, 4);
+	dhelper = new DirectionalLightHelper(directionalLight, 5, 0xff0000);
+	hemisphereLight = new HemisphereLight(0xffffff, 0xffffff, 0.4);
+	hemisphereLight.position.set(0, 8, 0);
+	hHelper = new HemisphereLightHelper(hemisphereLight, 5);
+	scene.add(directionalLight);
+	scene.add(hemisphereLight);
+};
+
+// 创建场景
+const setScene = () => {
+	scene = new Scene();
+	renderer = new WebGLRenderer();
+	renderer.setSize(innerWidth, innerHeight);
+	document.querySelector('.boxs').appendChild(renderer.domElement);
+};
+
+// 创建相机
+const setCamera = () => {
+	const { x, y, z } = defaultMap;
+	camera = new PerspectiveCamera(60, innerWidth / innerHeight, 1, 1000);
+	camera.position.set(x, y, z);
+};
+
+// 设置模型控制
+const setControls = () => {
+	controls = new OrbitControls(camera, renderer.domElement);
+	controls.maxPolarAngle = (0.9 * Math.PI) / 2;
+	controls.enableZoom = true;
+	controls.addEventListener('change', render);
+};
+
+//返回坐标信息
+const render = () => {
+	map.x = Number.parseInt(camera.position.x);
+	map.y = Number.parseInt(camera.position.y);
+	map.z = Number.parseInt(camera.position.z);
+};
+
+// 循环场景 、相机、 位置更新
+const loop = () => {
+	requestAnimationFrame(loop);
+	renderer.render(scene, camera);
+	controls.update();
+};
+
+//是否自动转动
+const isAutoFun = () => {
+	controls.autoRotate = true;
+};
+//停止转动
+const stop = () => {
+	controls.autoRotate = false;
+};
+
+//设置车身颜色
+const setCarColor = (index) => {
+	const currentColor = new Color(colorAry[index]);
+	scene.traverse((child) => {
+		if (child.isMesh) {
+			console.log(child.name);
+			if (child.name.includes('door_')) {
+				child.material.color.set(currentColor);
+			}
+		}
+	});
+};
+
+const loadFile = (url) => {
+	return new Promise((resolve, reject) => {
+		loader.load(
+			url,
+			(gltf) => {
+				resolve(gltf);
+			},
+			({ loaded, total }) => {
+				let load = Math.abs((loaded / total) * 100);
+				loadingWidth.value = load;
+				if (load >= 100) {
+					setTimeout(() => {
+						isLoading.value = false;
+					}, 1000);
+				}
+				console.log((loaded / total) * 100 + '% loaded');
+			},
+			(err) => {
+				reject(err);
+			}
+		);
+	});
+};
+
+//初始化所有函数
+const init = async () => {
+	setScene();
+	setCamera();
+	setLight();
+	setControls();
+	const gltf = await loadFile('src/assets/3d/scene.gltf');
+	scene.add(gltf.scene);
+	loop();
+};
+//用vue钩子函数调用
+onMounted(init);
+</script>
+
+<style>
+body {
+	margin: 0;
+}
+
+/* stylelint-disable-next-line selector-class-pattern */
+.maskLoading {
+	position: fixed;
+	inset: 0;
+	z-index: 1111111;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: #fff;
+	background: #000;
+}
+
+/* stylelint-disable-next-line selector-class-pattern */
+.maskLoading .loading {
+	width: 400px;
+	height: 20px;
+	overflow: hidden;
+	background: #000;
+	border: 1px solid #fff;
+	border-radius: 10px;
+}
+
+/* stylelint-disable-next-line selector-class-pattern */
+.maskLoading .loading div {
+	width: 0;
+	height: 20px;
+	background: #fff;
+	transition-timing-function: ease-in;
+	transition-duration: 500ms;
+}
+
+canvas {
+	width: 100%;
+	height: 100%;
+	margin: auto;
+}
+
+.mask {
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	width: 100%;
+	color: #fff;
+}
+
+.flex {
+	display: flex;
+	flex-wrap: wrap;
+	padding: 20px;
+}
+
+/* stylelint-disable-next-line no-descending-specificity */
+.flex div {
+	width: 10px;
+	height: 10px;
+	margin: 5px;
+	cursor: pointer;
+}
+</style>
