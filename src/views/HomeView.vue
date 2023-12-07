2ef18428c2340460c95101343a4e0bb809130e37
lirh42 <undercurre@163.com>
2023-12-07 20:20:22 +0800

    feat(views): 初步VR完成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,89 @@
 <script setup lang="ts">
 import * as THREE from 'three';
-import * as TWEEN from 'tween.js';
-import * as cpac5 from '@/assets/3d/cpac5.json';
-import * as cpbook2 from '@/assets/3d/cpbook2.json';
-import * as cpgame3 from '@/assets/3d/cpgame3.json';
-import * as cpkv3 from '@/assets/3d/cpkv3.json';
-import * as cpmovie4 from '@/assets/3d/cpmovie4.json';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import livingRoom from '@/assets/3d/livingRoom.jpg';
+import kitchen from '@/assets/3d/kitchen.jpg';
+// import gsap from 'gsap';
 
-// 数据集
-const db: Record<string, number[]> = {
-	ac: cpac5.vertices,
-	book: cpbook2.vertices,
-	game: cpgame3.vertices,
-	kv: cpkv3.vertices,
-	movie: cpmovie4.vertices
-};
+const dataList = [
+	{
+		image: livingRoom,
+		tipsList: [
+			{
+				position: { x: -200, y: -4, z: -147 },
+				content: {
+					title: '进入厨房',
+					text: '',
+					image: 1,
+					showTip: false,
+					showTitle: true
+				}
+			},
+			{
+				position: { x: -100, y: 0, z: -231 },
+				content: {
+					title: '信息点2',
+					text: '77989',
+					showTip: true,
+					showTitle: false
+				}
+			},
+			{
+				position: { x: 150, y: -50, z: -198 },
+				content: {
+					title: '信息点3',
+					text: 'qwdcz',
+					showTip: true,
+					showTitle: false
+				}
+			},
+			{
+				position: { x: 210, y: 11, z: -140 },
+				content: {
+					title: '信息点4',
+					text: '大豆食心虫侦察十大大苏打大大大大大大大',
+					showTip: true,
+					showTitle: false
+				}
+			},
+			{
+				position: { x: 208, y: -12, z: 140 },
+				content: {
+					title: '信息点5',
+					text: 'eq',
+					showTip: true,
+					showTitle: false
+				}
+			},
+			{
+				position: { x: 86, y: -9, z: 236 },
+				content: {
+					title: '进入房间',
+					text: '',
+					showTip: false,
+					showTitle: true
+				}
+			}
+		]
+	},
+	{
+		image: kitchen,
+		tipsList: [
+			{
+				position: { x: -199, y: -24, z: 145 },
+				content: {
+					title: '进入大厅',
+					text: '',
+					image: 0,
+					showTip: false,
+					showTitle: true
+				}
+			}
+		]
+	}
+];
 
-// 当前使用的数据集
-let curData = 'ac';
-
-// 当前的点阵数据
-let curV: number[] = db['ac'];
+const curIndex = 0;
 
 // 容器
 const canvasContainer = ref<HTMLElement | null>(null);
@@ -29,22 +92,21 @@ const canvasContainer = ref<HTMLElement | null>(null);
 let scene: THREE.Scene,
 	renderer: THREE.WebGLRenderer,
 	camera: THREE.Camera,
-	geometry: THREE.BufferGeometry,
-	material: THREE.PointsMaterial,
-	particles: THREE.Points;
+	controls: OrbitControls,
+	sphereGeometry: THREE.SphereGeometry,
+	sphere: THREE.Mesh;
 
 // 相机初始(默认)坐标
 const defaultMap = {
-	x: 0,
+	x: 50,
 	y: 0,
-	z: 10
+	z: 40
 };
 
 // 创建场景和渲染器
 const setScene = () => {
 	scene = new THREE.Scene();
-	//雾化
-	scene.fog = new THREE.FogExp2(0x05050c, 0.0005);
+	scene.background = new THREE.Color(0x101010);
 	renderer = new THREE.WebGLRenderer();
 	// 设置像素比例同步，让高性能显示更漂亮
 	renderer.setPixelRatio(window.devicePixelRatio);
@@ -57,90 +119,50 @@ const setScene = () => {
 // 创建相机
 const setCamera = () => {
 	const { x, y, z } = defaultMap;
-	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 	camera.position.set(x, y, z);
-	// 调整朝向
-	camera.lookAt(0, 0, 0);
 };
 
-// 创建粒子几何体
-const setPointGeometry = () => {
-	geometry = new THREE.BufferGeometry();
-	// 加载贴图材质
-	const textureLoader = new THREE.TextureLoader();
-	const mapDot = textureLoader.load('/src/assets/3d/gradient.png');
-	material = new THREE.PointsMaterial({
-		size: 0.02,
-		sizeAttenuation: true,
-		color: 0xffffff,
-		transparent: true,
-		opacity: 1,
-		map: mapDot
-	});
-	const vertices = [];
-	const particleSum = 15000;
-	const longestDistance = 1000;
-
-	for (let i = 0; i < particleSum; i++) {
-		const x = getRangeRandom(-1 * longestDistance, longestDistance);
-		const y = getRangeRandom(-1 * longestDistance, longestDistance);
-		const z = getRangeRandom(-1 * longestDistance, longestDistance);
-		vertices.push(x, y, z);
-	}
-
-	geometry.setAttribute('position', new THREE.Float32BufferAttribute(curV, 3));
-	particles = new THREE.Points(geometry, material);
-	scene.add(particles);
+// 设置模型控制
+const setControls = () => {
+	controls = new OrbitControls(camera, renderer.domElement);
+	controls.minDistance = 1;
+	controls.maxDistance = 100;
+	controls.enablePan = false;
 };
 
-function getRangeRandom(e: number, t: number) {
-	return Math.random() * (t - e) + e;
-}
+// 创建几何体
+// 全屋VR在于把环境贴图作用于球体，然后从内部看
+const setContent = () => {
+	sphereGeometry = new THREE.SphereGeometry(16, 50, 50);
+	sphereGeometry.scale(16, 16, -16);
+	let texture = new THREE.TextureLoader().load(dataList[curIndex].image);
+	let sphereMaterial = new THREE.MeshBasicMaterial({ map: texture });
+	sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	scene.add(sphere);
+};
 
 // 渲染函数
 const render = () => {
-	requestAnimationFrame(render);
-	TWEEN.update();
+	controls.update();
 	renderer.render(scene, camera);
+	renderer.sortObjects = false;
+	requestAnimationFrame(render);
 };
 
 // 初始化所有函数
 const init = async () => {
 	setScene();
 	setCamera();
-	setPointGeometry();
+	setControls();
+	setContent();
 	render();
 };
 
 //用vue钩子函数调用
 onMounted(init);
-
-function handleChange(key: string) {
-	curV.forEach(function (e, i) {
-		var length = db[key].length;
-		var o = db[key][i % length];
-		new TWEEN.Tween(e)
-			.to(o, 1000)
-			.easing(TWEEN.Easing.Exponential.In)
-			.delay(1000 * Math.random())
-			.onUpdate(() => {
-				curV[i] = o;
-				geometry.setAttribute('position', new THREE.Float32BufferAttribute(curV, 3));
-			})
-			.start();
-	});
-	// curData = key;
-	// curV = db[key];
-}
 </script>
 
 <template>
 	<div ref="canvasContainer"></div>
-	<div class="absolute right-0 top-0">
-		<button @click="handleChange('ac')">ac</button>
-		<button @click="handleChange('book')">book</button>
-		<button @click="handleChange('game')">game</button>
-		<button @click="handleChange('kv')">kv</button>
-		<button @click="handleChange('movie')">movie</button>
-	</div>
 </template>
